980e465501802d5506eeca3690cfc628ccb51293
T3nj1 <Anna@Anna-PC>
2013-10-23 18:27:45 +0200

    Methode in der Bar hinzugefügt

--- Spielobjekte/DiscoObjekte/Bar.java
+++ Spielobjekte/DiscoObjekte/Bar.java
@@ -1,9 +1,18 @@
-
-//created by Anna & Silke
+package edu.dhbw.pse.music;
 
 public class Bar extends DiscoObjects {
 
 	Drinks drinklist = new Drinks[];
 	int waitingtime;
 	accessible = 0;
+	
+	//Vielleicht wird Mensch p durch ein Attribut in der Klasse Mensch ersetzt
+	buydrink(key k, Mensch p){
+		boolean clickedbyplayer;
+		if(p=Spieler){
+			clickedbyplayer=1;
+		}
+		else clickedbyplayer=0;
+		Drinks[k].drinkbought(clickedbyplayer);
+	}
 }
