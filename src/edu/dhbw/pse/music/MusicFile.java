6264e2942266a84a8f2180df6e3651dbe7055bcd
Nicolas <studium@nmarin.de>
2013-10-23 16:29:24 +0200

    Music-Package
    Change-Id: Ie42a7dfb86335aca8bcd65b81d6eee67f2655d6c
    Signed-off-by: Nicolas <studium@nmarin.de>

---
+++ src/edu/dhbw/pse/music/MusicFile.java
@@ -0,0 +1,12 @@
+package edu.dhbw.pse.music;
+public class MusicFile {
+	public String category;
+	public String title;
+	public String path;
+
+	public MusicFile(String title, String category, String path) {
+		this.title = title;
+		this.category = category;
+		this.path = path;
+	}
+}
