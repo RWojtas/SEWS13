9550255b6b00b53f46e13401a919c5c8ea4d1c9a
Suriwary <Silke@Silke-PC>
2013-11-05 21:57:58 +0100

    Erstellung der methode setStatusAS

--- src/objects/DiscoObject.java
+++ src/objects/DiscoObject.java
@@ -37,6 +37,22 @@ public abstract class DiscoObject extends JLabel {
 		return position.getYPosition();
 	}
 	
+	/*
+	1. BlauHohn
+	2. RotOchsen
+	3. GelbVögln
+	4. SchwarzKatzerl
+	5. ZitronenLimonade
+	6. Eistee
+	7. Cocktail
+	8. Shot
+	9. sitzen
+	10. tanzen
+	11. flirten
+	12. auf Toilette gehen
+	*/
+	
+	
 	public void setStatusES(Human p, int aktivitaet, int number){
 		
 		if (aktivitaet == 3){
@@ -91,7 +107,67 @@ public abstract class DiscoObject extends JLabel {
 				case 12:	p.setUrine(0);
 							break;
 				
-			}			
+			}	
 		}
 	}
+			
+		public void setStatusAS (Human p, int aktivitaet, int number){
+			if (aktivitaet == 3){
+				switch (number){
+					case 0: 	p.addEnergy(0.30);
+								p.addUrine(0.40);
+								p.removeAlcLevel(0.20);
+								p.addFun(0.10);
+								h.setBonus(1);
+								break;
+					case 1: 	p.addFlirt(0.20);
+								p.addUrine(0.40);
+								p.addFun(0.10);
+								h.setBonus(1);
+								break;
+					case 2:		p.addFun(0.20);
+								p.addUrine(0.40);
+								p.addFlirt(0.20);
+								h.setBonus(1);
+								break;
+					case 3:		p.removeAlcLevel(0.20);
+								p.addEnergy(0.20);
+								p.addUrine(0.40);
+								p.addFun(0.1);
+								h.setBonus(1);
+								break;
+					case 4: 	p.removeAlcLevel(0.20);
+								p.addEnergy(0.20);
+								p.addUrine(0.40);
+								p.addFun(0.10);
+								h.setBonus(1);
+								break;
+					case 5: 	p.addUrine(0.40);
+								break;
+					case 6:		p.addUrine(0.40);
+								p.addEnergy(0.30);
+								break;
+					case 7:		p.addUrine(0.40);
+								p.addAlcLevel(0.30);
+								break;
+					case 8:		p.addUrine(0.40);
+								p.addAlcLevel(0.30);
+								break;
+					case 9:		p.addEnergy(0.40);
+								break;
+					case 10 :	p.removeEnergy(0.25);
+								p.addFun(0.3);
+								break;
+					case 11: 	p.addFlirt(30);
+								p.addFun(30);
+								break;
+					case 12:	p.setUrine(0);
+								break;
+					
+				}	
+			}
+			
+		}
+	
+	
 }
